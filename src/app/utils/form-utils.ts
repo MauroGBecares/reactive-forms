02531c3af6e741d4ef95a3834f69eac6fd99594b
MauroGBecares <maurobecares26@gmail.com>
2026-03-10 16:32:30 -0300

    nuevos cambios

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,9 +1,21 @@
-import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+import { email } from "@angular/forms/signals";
+
+async function sleep(){
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true)
+    }, 2500)
+  })
+}
 
 export class FormUtils {
   // Expresiones Regulares
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
-  static getTextError(errors: ValidationErrors){
+  static getTextError(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -14,6 +26,17 @@ export class FormUtils {
           return `Valor mínimo de ${errors['min'].min}`;
         case 'email':
           return 'El email ingresado no es válido'
+        case 'emailTaken':
+          return 'El email ya esta siendo usado por otro usuario'
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return 'El email no tiene el formato válido'
+          }
+          return 'Error de patrón contra expresión regular'
+        case 'noStrider':
+          return 'No puede ser usado ese nombre por las políticas de la empresa'
+        default:
+          return `Error de validación no controlado ${key}`
       }
     }
     return null;
@@ -24,6 +47,15 @@ export class FormUtils {
       !!form.controls[fieldName].errors
       && form.controls[fieldName].touched
     )
+  }
+
+  static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value
+      const field2Value = formGroup.get(field2)?.value
+
+      return field1Value === field2Value ? null : { passwordsNotEqual: true }
+    }
   }
 
   static getFieldError(form: FormGroup, fieldName: string): string | null {
@@ -48,5 +80,31 @@ export class FormUtils {
     const errors = formArray.controls[index].errors ?? {}
 
     return FormUtils.getTextError(errors)
+  }
+
+  // Primero valida las validaciones sincronas y luego las asincronas. Si la validaciones sincronas no las pasa, no corre esta funcion
+  static async checkingServerResponse(control: AbstractControl) : Promise<ValidationErrors | null> {
+    await sleep() // 2 segundos y medio
+
+    const formValue = control.value;
+
+    if(formValue === 'hola@mundo.com'){
+      return {
+        emailTaken: true
+      };
+    }
+    return null;
+  }
+
+  static notStrider(control: AbstractControl) : ValidationErrors | null {
+    const formValue = control.value;
+
+    if(formValue === 'strider'){
+      return {
+        noStrider: true
+      }
+    }
+
+    return null;
   }
 }
